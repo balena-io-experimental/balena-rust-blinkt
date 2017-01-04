09e2d0ddd04ec055658e5d1741f7fa3ba73107f6
Gergely Imreh <imrehg@gmail.com>
2017-01-04 18:35:45 +0000

    deploying Blinkt! Rust demo

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use std::{thread, time};
-use std::env;
+extern crate blinkt;
+
+use std::{thread, mem};
+use std::time::Duration;
+
+use blinkt::Blinkt;
 
 fn main() {
-	let ten_sec = time::Duration::from_millis(10_000);
-	let key = "FOO";
+    let mut blinkt = Blinkt::new().unwrap();
+    let (red, green, blue) = (&mut 255, &mut 0, &mut 0);
 
-	while true {
+    loop {
+        blinkt.set_all_pixels(*red, *green, *blue);
+        blinkt.show();
 
-		match env::var(key) {
-		    Ok(val) => println!("{}: {:?}", key, val),
-		    Err(e) => println!("couldn't interpret {}: {}", key, e),
-		}
-		thread::sleep(ten_sec);
-		println!("Hello, world!");
-	}
-    
+        thread::sleep(Duration::from_millis(250));
+
+        mem::swap(red, green);
+        mem::swap(red, blue);
+    }
 }
